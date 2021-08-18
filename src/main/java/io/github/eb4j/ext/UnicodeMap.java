3f95496694ca4eea45bf9568a2a6e662ab05b3ab
Hiroshi Miura <miurahr@linux.com>
2021-08-18 11:40:03 +0900

    Use single import par class
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/io/github/eb4j/ext/UnicodeMap.java
+++ src/main/java/io/github/eb4j/ext/UnicodeMap.java
@@ -2,7 +2,11 @@ package io.github.eb4j.ext;
 
 import io.github.eb4j.EBException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
